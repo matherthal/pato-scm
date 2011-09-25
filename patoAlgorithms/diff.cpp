6dcee8066eb93d09570c3eb1e6847745b6806d32
hbarbalho <hbarbalho@localhost>
2011-09-25 14:32:14 -0300

    Implementacao inicial do Diff.

--- patoAlgorithms/diff.cpp
+++ patoAlgorithms/diff.cpp
@@ -4,6 +4,7 @@ Diff::Diff(ifstream *fileA,ifstream *fileB)
 {
     Diff::fileA = fileA;
     Diff::fileB = fileB;
+    calculateDiff();
 }
 
 Diff::Diff(char *fileNameA,char *fileNameB)
@@ -13,14 +14,26 @@ Diff::Diff(char *fileNameA,char *fileNameB)
     Diff::fileB = &fileB;
     Diff::fileA->open(fileNameA,fstream::binary);
     Diff::fileB->open(fileNameB,fstream::binary);
+    calculateDiff();
+}
+
+Diff::~Diff(){
+    Diff::fileA->close();
+    Diff::fileB->close();
 }
 
 void Diff::calculateDiff(){
     if(fileA->is_open() && fileB->is_open()){
-
+        char dataA,dataB;
+        empty = true;
+        while(!fileA->eof() && !fileB->eof()){
+            fileA->read(&dataA,1);
+            fileB->read(&dataB,1);
+            if(dataA != dataB)empty = false;
+        }
     }
 }
 
 bool Diff::isEmpty(){
-    return false;
+    return empty;
 }
