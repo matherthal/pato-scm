7bbc8efacd1b3d6a4ac1c9b47785a5467a33ad7e
david.brto <david.brto@localhost>
2011-10-01 19:10:57 -0300

    another functions added

--- patoDataModel/patodatamodel.h
+++ patoDataModel/patodatamodel.h
@@ -18,8 +18,22 @@ public:
     static PatoDataModel* getInstance();
     static void destroyInstance();
 
-    //getters and setters
-    int getFileKey(QString pFilePath);
+    //getters and setters >
+    int getFileKey(const string& pFilePath);
+
+    int getLastVersion(const string& path);
+    //<
+
+    //repositoy operations >
+
+    //version's default value -> 0 (latest version)
+    //the idea is return an array of int (all file keys)
+    int* checkout(const string& path, int version = 0);
+
+    //we need to define a data structure to represent the file!
+    int checkin(unsigned char file[], const string& path);
+
+    //<
 
     //user operations >
     bool validateUser(const string& login, const string& password);
@@ -28,8 +42,6 @@ public:
     //project operations>
     bool validateProject( const string& projectName );
     //<
-
-
 };
 
 #endif // PATODATAMODEL_H
