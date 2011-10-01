d49b49eb998dd958258f330ce751ac735066f9b7
hbarbalho <hbarbalho@gmail.com>
2011-10-01 11:10:39 -0300

    co-autor:Daniel Martins.Diff usando lcs mas sem otimizacao e s? imprimindo na tela

--- patoAlgorithms/diff.cpp
+++ patoAlgorithms/diff.cpp
@@ -2,15 +2,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdlib.h>
 
-Diff::Diff(ifstream *fileA,ifstream *fileB)
+Diff::Diff(ifstream *fileA,ifstream *fileB, int _type)
 {
     Diff::fileA = fileA;
     Diff::fileB = fileB;
+    type = _type;
     calculateDiff();
 }
 
-Diff::Diff(const char *fileNameA,const char *fileNameB)
+Diff::Diff(const char *fileNameA,const char *fileNameB, int _type)
 {
     ifstream fileA,fileB;
     struct stat results;
@@ -22,6 +24,7 @@ Diff::Diff(const char *fileNameA,const char *fileNameB)
     Diff::fileB = &fileB;
     Diff::fileA->open(fileNameA,fstream::binary);
     Diff::fileB->open(fileNameB,fstream::binary);
+    type = _type;
     calculateDiff();
 }
 
@@ -37,22 +40,92 @@ void Diff::calculateDiff(){
         dataB = new char[sizeFileB];
         fileA->read(dataA,sizeFileA);
         fileB->read(dataB,sizeFileB);
-        empty = lcs(dataA,0,dataB,0).length()==0;
+        char *lcs_ret = lcs(dataA,dataB,type);
+        cout<<lcs_ret<<endl;
+        empty = sizeFileA == sizeFileB && strlen(lcs_ret)==sizeFileA;
     }
 }
 
-string Diff::lcs(char *lA, int iA,char *lB,int iB){
-    cout << iA<< " "<<iB<<endl;
-    if(iA==strlen(lA) || iB == strlen(lB))
-        return string("");
-    if(lA[iA]==lB[iB]){
-        string ret("");
-        ret = lA[iA];
-        return ret.append(lcs(lA,iA+1,lB,iB+1));
+char* Diff::lcsBin(char *arqA,unsigned long a, char *arqB,unsigned long b){
+    if(strlen(arqA)==a || strlen(arqB)==b){
+            return (char*)"";
+    }else if(arqA[a]==arqB[b]){
+            char *ret,*tmp;
+
+            tmp = lcsBin(arqA,a+1,arqB,b+1);
+            ret = (char*)malloc(sizeof(char)*(strlen(tmp)+1));
+            ret[0] = arqA[a];
+            strcat(ret,tmp);
+
+            return ret;
+    }else{
+            char *p1,*p2;
+            p1 = lcsBin(arqA,a+1,arqB,b);
+            p2 = lcsBin(arqA,a,arqB,b+1);
+            if(strlen(p2)>strlen(p1)){
+                    return p2;
+            }else{
+                    return p1;
+            }
     }
-    string s1 = lcs(lA,iA+1,lB,iB);
-    string s2 = lcs(lA,iA,lB,iB+1);
-    return s1.length()>s2.length()?s1:s2;
+}
+
+char* Diff::lcsTxt(char *arqA,unsigned long a, char *arqB,unsigned long b){
+
+    if(strlen(arqA)==a || strlen(arqB)==b){
+            //cout <<"*";
+            return (char*)"\0";
+    }
+
+    int i=0;
+    int fimA,fimB,iguais;
+    fimA = fimB = 0;
+    iguais = 1;
+    while(!fimA || !fimB){
+            if(!fimA && !fimB){
+                    if(arqA[a+i] != arqB[b+i])
+                            iguais = 0;
+                    if(arqA[a+i]=='\n'||arqA[a+i]=='\0')fimA=a+i;
+                    if(arqB[b+i]=='\n'||arqB[b+i]=='\0')fimB=b+i;
+            }else if(fimA){
+                    if(arqB[b+i]=='\n'||arqB[b+i]=='\0')fimB=b+i;
+            }	else if(fimB){
+                    if(arqA[a+i]=='\n'||arqA[a+i]=='\0')fimA=a+i;
+            }
+            i++;
+    }
+
+    if(iguais)
+    {
+            char *ret,*tmp;
+            tmp = lcsTxt(arqA,fimA+1,arqB,fimB+1);
+            ret = (char*)malloc(sizeof(char)*(strlen(tmp)+(fimA-a+1)));
+            for(int j=a;j<fimA+1;j++)
+                    ret[j-a] = arqA[j];
+            //printf("%s%s==",ret,tmp);
+            strcat(ret,tmp);
+            //printf("%s?\n",ret);
+            return ret;
+
+    }else{
+            char *p1,*p2;
+            p1 = lcsTxt(arqA,fimA+1,arqB,b);
+            p2 = lcsTxt(arqA,a,arqB,fimB+1);
+            if(strlen(p2)>strlen(p1)){
+                    return p2;
+            }else{
+                    return p1;
+            }
+    }
+}
+
+char* Diff::lcs(char *arqA, char *arqB, int type){
+    char *tmp;
+    if (type == T_Bin)
+            tmp = lcsBin(arqA,0,arqB,0);
+    else
+            tmp = lcsTxt(arqA,0,arqB,0);
+    return tmp;
 }
 
 bool Diff::isEmpty(){
