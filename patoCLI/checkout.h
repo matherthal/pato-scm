0f0ba6947b9c176ba56b023045c3437b29263eb7
marapao <marapao@marapao-desktop>
2011-10-03 20:50:31 -0300

    Initial version of commit CLI

---
+++ patoCLI/checkout.h
@@ -0,0 +1,41 @@
+/* 
+ * File:   checkout.h
+ * Author: marapao
+ *
+ * Created on October 3, 2011, 8:19 PM
+ */
+#include<string>
+using namespace std;
+
+#ifndef CHECKOUT_H
+#define	CHECKOUT_H
+
+
+
+class checkout {
+public:
+    checkout();
+    checkout(const checkout& orig);
+    virtual ~checkout();
+    void command(int argc, char** argv);
+    void setPassword(string password);
+    string getPassword() const;
+    void setUsername(string username);
+    string getUsername() const;
+    void setAddress(string address);
+    string getAddress() const;
+    void setWorkspace(string workspace);
+    string getWorkspace() const;
+    void setRevision(int revision);
+    int getRevision() const;
+private:
+    int revision;
+    string workspace;
+    string address;
+    string username;
+    string password;
+
+};
+
+#endif	/* CHECKOUT_H */
+
