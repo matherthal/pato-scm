643aed765fe4bff6b8b2f40bf52e3f25723b3306
david.brto <david.brto@localhost>
2011-10-06 15:14:01 -0300

    Trying to add DB access

--- patoFS/patofs.h
+++ patoFS/patofs.h
@@ -2,10 +2,21 @@
 #define PATOFS_H
 
 #include "PatoFS_global.h"
+#include <QtSql>
 
 class PATOFSSHARED_EXPORT PatoFS {
-public:
+
+private:
     PatoFS();
+    ~PatoFS();
+
+    static PatoFS* mPatoFS;
+    QSqlDatabase db;
+
+public:
+
+	static PatoFS* getInstance();
+	static vod destroyInstance();
 };
 
 #endif // PATOFS_H
