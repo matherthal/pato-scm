d24492636072ad01060bcc07c480b81837348a3a
hbarbalho <hbarbalho@gmail.com>
2011-10-08 02:24:50 -0300

    LCS using dynamic programming and a hash key to compare lines

--- patoAlgorithms/diff.cpp
+++ patoAlgorithms/diff.cpp
@@ -4,6 +4,61 @@
 #include <string.h>
 #include <stdlib.h>
 int Diff::use_pd;
+
+unsigned int func_hash(char* str, unsigned int len)
+{
+   unsigned int hash = 0;
+   unsigned int x    = 0;
+   unsigned int i    = 0;
+
+   for(i = 0; i < len; str++, i++)
+   {
+      hash = (hash << 4) + (*str);
+      if((x = hash & 0xF0000000L) != 0)
+      {
+         hash ^= (x >> 24);
+      }
+      hash &= ~x;
+   }
+
+   return hash;
+}
+
+char* init_str(char *str,unsigned int from,unsigned int *to){
+    if(str[*to]=='\n'){
+        unsigned int length = *to-from;
+        char *tmp = (char*)malloc(sizeof(char)*(length+1));
+        tmp[length]='\0';
+        return tmp;
+    }else{
+        unsigned int posTmp = *to-from;
+        unsigned int posStr = *to;
+        *to = *to+1;
+        char* tmp = init_str(str,from,to);
+        tmp[posTmp]=str[posStr];
+        return tmp;
+    }
+}
+
+hashtable** Diff::init_hash(char *arq,int size,unsigned int i,int *length){
+    if(i>=size){
+        hashtable** _htable =(hashtable**)malloc(sizeof(hashtable*)* (*length));
+        return _htable;
+    }else{
+        unsigned int pos=i;
+        char *str = init_str(arq,i,&pos);
+        unsigned int str_length = pos-i;
+        int my_pos = *length;
+        *length = *length+1;
+        hashtable** _htable = init_hash(arq,size,pos+1,length);
+        _htable[my_pos] = (hashtable*)malloc(sizeof(hashtable));
+        _htable[my_pos]->line = str;
+        _htable[my_pos]->length=str_length;
+        _htable[my_pos]->hash = func_hash(str,str_length);
+        return _htable;
+    }
+}
+
 Diff::Diff(ifstream *fileA,ifstream *fileB, int _type)
 {
     Diff::fileA = fileA;
@@ -26,13 +81,11 @@ Diff::Diff(const char *fileNameA,const char *fileNameB, int _type)
     Diff::fileB->open(fileNameB,fstream::binary);
     type = _type;
     calculateDiff();
-		
+    Diff::fileA->close();
+    Diff::fileB->close();
 }
 
-Diff::~Diff(){
-    //Diff::fileA->close();
-    //Diff::fileB->close();
-		
+Diff::~Diff(){	
 }
 
 void Diff::calculateDiff(){
@@ -42,189 +95,154 @@ void Diff::calculateDiff(){
         dataB = new char[sizeFileB];
         fileA->read(dataA,sizeFileA);
         fileB->read(dataB,sizeFileB);
-        char *lcs_ret = lcs(dataA,dataB,type);
-        cout<<lcs_ret<<endl;
-        empty = sizeFileA == sizeFileB && strlen(lcs_ret)==sizeFileA;
-				//free(lcs_ret);
+        sizeHashA=0,sizeHashB=0;
+        htableA = init_hash(dataA,sizeFileA,0,&sizeHashA);
+        htableB = init_hash(dataB,sizeFileB,0,&sizeHashB);
+        if(use_pd)table = build_linked_table();
+
+        int length_lcs = 0;
+        t_lcs *p= lcsTxt(0,0,&length_lcs);
+
+        /*
+        printf("\nLCS:\n");
+        while(p!=NULL){
+            printf("%s\n",htableA[p->index]->line);
+            p = p->next;
+        }*/
+        empty = sizeHashA==sizeHashB && length_lcs==sizeHashA;
+        if(use_pd)free_table(table,sizeHashA,sizeHashB);
+        free_hash(htableA,sizeHashA);
+        free_hash(htableB,sizeHashB);
     }
 }
 
 char* Diff::lcsBin(char *arqA,unsigned long a, char *arqB,unsigned long b){
     if(strlen(arqA)==a || strlen(arqB)==b){
-            return (char*)"";
+        return (char*)"";
     }else if(arqA[a]==arqB[b]){
-            char *ret,*tmp;
+        char *ret,*tmp;
 
-            tmp = lcsBin(arqA,a+1,arqB,b+1);
-            ret = new char[strlen(tmp)+1];//(char*)malloc(sizeof(char)*(strlen(tmp)+1));
-            ret[0] = arqA[a];
-            strcat(ret,tmp);
+        tmp = lcsBin(arqA,a+1,arqB,b+1);
+        ret = new char[strlen(tmp)+1];//(char*)malloc(sizeof(char)*(strlen(tmp)+1));
+        ret[0] = arqA[a];
+        strcat(ret,tmp);
 
-            return ret;
+        return ret;
     }else{
-            char *p1,*p2;
-            p1 = lcsBin(arqA,a+1,arqB,b);
-            p2 = lcsBin(arqA,a,arqB,b+1);
-            if(strlen(p2)>strlen(p1)){
-                    return p2;
-            }else{
-                    return p1;
-            }
+        char *p1,*p2;
+        p1 = lcsBin(arqA,a+1,arqB,b);
+        p2 = lcsBin(arqA,a,arqB,b+1);
+        if(strlen(p2)>strlen(p1)){
+                return p2;
+        }else{
+                return p1;
+        }
     }
 }
 
-char *cat_plus(biglinkedtable *p,int length){
-	if(p==NULL){
-		char *ret = (char*)malloc(sizeof(char)*length);
-		ret[0]='\0';
-		return ret;
-	}else{
-		return strncat(cat_plus(p->next,length+p->length),p->add,length);
-	}
+void Diff::add_to_table(int i,int j,int index,int next_i,int next_j){
+    if(!use_pd)return;
+    if(table[i]==NULL){
+        table[i]=(biglinkedtable**)malloc(sizeof(biglinkedtable*)*sizeHashB);
+        memset(table[i],NULL,sizeof(biglinkedtable*)*sizeHashB);
+    }
+    table[i][j] = (biglinkedtable*)malloc(sizeof(biglinkedtable));
+    table[i][j]->index = index;
+    if(table[next_i]!=NULL && next_i<sizeHashA && next_j<sizeHashB){
+        table[i][j]->next = table[next_i][next_j];
+    }else
+        table[i][j]->next=NULL;
 }
 
-char* Diff::lcsTxt(char *arqA,unsigned long a, char *arqB,unsigned long b){
-		if(use_pd){
-			if(table[a]!=NULL && table[a][b]!=NULL){
-				char *tmp = NULL;
-				biglinkedtable *p = table[a][b]->next;
-				tmp = cat_plus(p,0);
-				char *ret = new char[table[a][b]->length];
-				strcpy(ret,table[a][b]->add);
-				strcat(ret,tmp);
-				return ret;
-			}
-		}
-    if(strlen(arqA)<=a || strlen(arqB)<=b){
-            //cout <<"*";
-            return (char*)"\0";
+t_lcs* Diff::lcsTxt(int i,int j,int *size){
+    if(i>=sizeHashA || j>=sizeHashB){
+        *size=0;
+        return NULL;
     }
-
-    int i=0;
-    int fimA,fimB,iguais;
-    fimA = fimB = 0;
-    iguais = 1;
-    while(!fimA || !fimB){
-            if(!fimA && !fimB){
-                    if(arqA[a+i] != arqB[b+i])
-                            iguais = 0;
-                    if(arqA[a+i]=='\n'||arqA[a+i]=='\0')fimA=a+i;
-                    if(arqB[b+i]=='\n'||arqB[b+i]=='\0')fimB=b+i;
-            }else if(fimA){
-                    if(arqB[b+i]=='\n'||arqB[b+i]=='\0')fimB=b+i;
-            }	else if(fimB){
-                    if(arqA[a+i]=='\n'||arqA[a+i]=='\0')fimA=a+i;
+    if(use_pd && table[i]!=NULL && table[i][j]!=NULL){
+        t_lcs *n=NULL,*ret=NULL;
+        biglinkedtable *pt = table[i][j];
+        while(pt!=NULL){
+            if(pt->index>=0){
+                n = (t_lcs*)malloc(sizeof(t_lcs));
+                if(ret==NULL)ret = n;
+                n->index = pt->index;
+                n->next = NULL;
+                n = n->next;
             }
-            i++;
+            pt = pt->next;
+        }
+        return ret;
     }
-
-    if(iguais)
-    {
-            char *ret,*tmp;
-            tmp = lcsTxt(arqA,fimA+1,arqB,fimB+1);
-            ret = new char[strlen(tmp)+(fimA-a+1)];//(char*)malloc(sizeof(char)*(strlen(tmp)+(fimA-a+1)));
-						if(ret==NULL)printf("not malloced\n");
-            for(int j=a;j<fimA+1;j++)
-                  ret[j-a] = arqA[j];
-						if(use_pd){
-							if(table[a]==NULL){
-								table[a] = (biglinkedtable**)malloc(sizeof(biglinkedtable*)*(sizeFileB+1));
-								memset(table[a], NULL, sizeof(biglinkedtable**)*(sizeFileB+1));
-							}
-							table[a][b] =  (biglinkedtable*)malloc(sizeof(biglinkedtable));
-							table[a][b]->length = strlen(tmp)+(fimA-a+1);
-							table[a][b]->add = new char[table[a][b]->length];
-							table[a][b]->i = a;
-							table[a][b]->j = b;
-							strcpy(table[a][b]->add,ret);
-							if(table[fimA+1]!=NULL && table[fimA+1][fimB+1] !=NULL)
-								table[a][b]->next = table[fimA+1][fimB+1];
-							else
-								table[a][b]->next = NULL;
-						}
-            strcat(ret,tmp);
-            return ret;
-
+    int same = 0;
+    if(htableA[i]->hash == htableB[j]->hash){
+        if(htableA[i]->length==htableB[j]->length)
+            same = strcmp(htableA[i]->line,htableB[j]->line)==0;
+    }
+    if(same){
+        t_lcs *p = lcsTxt(i+1,j+1,size);
+        *size = *size+1;
+        t_lcs *n = (t_lcs*)malloc(sizeof(t_lcs));
+        n->index = i;
+        n->next = p;
+        add_to_table(i,j,n->index,i+1,j+1);
+        return n;
     }else{
-            char *p1,*p2;
-            p1 = lcsTxt(arqA,fimA+1,arqB,b);
-            p2 = lcsTxt(arqA,a,arqB,fimB+1);
-            if(strlen(p2)>strlen(p1)){
-										if(use_pd){
-											if(table[a]==NULL){
-												table[a] = (biglinkedtable**)malloc(sizeof(biglinkedtable*)*(sizeFileB+1));
-												memset(table[a], NULL, sizeof(biglinkedtable**)*(sizeFileB+1));
-											}
-
-											table[a][b] =  (biglinkedtable*)malloc(sizeof(biglinkedtable));
-											table[a][b]->length = 0;
-											table[a][b]->add = new char[1];
-											table[a][b]->i = a;
-											table[a][b]->j = b;
-											strcpy(table[a][b]->add,"");
-											if(table[fimA+1]!=NULL && table[fimA+1][b] !=NULL)
-												table[a][b]->next = table[fimA+1][b];
-											else
-												table[a][b]->next = NULL;
-										}
-                    return p2;
-            }else{
-										if(use_pd){
-											if(table[a]==NULL){
-												table[a] = (biglinkedtable**)malloc(sizeof(biglinkedtable*)*(sizeFileB+1));
-												memset(table[a], NULL, sizeof(biglinkedtable**)*(sizeFileB+1));
-											}
-											table[a][b] =  (biglinkedtable*)malloc(sizeof(biglinkedtable));
-											table[a][b]->length = 0;
-											table[a][b]->add = new char[1];
-											table[a][b]->i = a;
-											table[a][b]->j = b;
-											strcpy(table[a][b]->add,"");
-											if(table[a]!=NULL && table[a][fimB+1] !=NULL)
-												table[a][b]->next = table[a][fimB+1];
-											else
-												table[a][b]->next = NULL;
-										}
-                    return p1;
-            }
+        int size1=*size;
+        int size2=*size;
+        t_lcs *p1 = lcsTxt(i+1,j,&size1);
+        t_lcs *p2 = lcsTxt(i,j+1,&size2);
+        if(size1>size2){
+            add_to_table(i,j,-1,i+1,j);
+            *size = size1;
+            free_lcs(p2);
+            return p1;
+        }else{
+            add_to_table(i,j,-1,i,j+1);
+            *size = size2;
+            free_lcs(p1);
+            return p2;
+        }
     }
 }
 
-void Diff::free_table(biglinkedtable*** table){
-	if(table!=NULL){
-		for(unsigned int i=0;i<sizeFileA+1;i++){
-			if(table[i]!=NULL){
-				for(unsigned int j=0;j<sizeFileB+1;j++){
-					if(table[i][j]!=NULL){
-						free(table[i][j]->add);
-						table[i][j]->next = NULL;
-						free(table[i][j]);
-					}
-				}
-				free(table[i]);
-			}
-		}
-		free(table);
-	}
+biglinkedtable*** Diff::build_linked_table(){
+    table = (biglinkedtable***)malloc(sizeof(biglinkedtable**)*sizeHashA);
+    memset(table,NULL,sizeof(biglinkedtable**)*sizeHashA);
 }
 
-char* Diff::lcs(char *arqA, char *arqB, int type){
+void Diff::free_lcs(t_lcs *p){
+    if(p!=NULL && p->next==NULL)
+        free(p);
+    else if(p!=NULL){
+        free_lcs(p->next);
+        p->next=NULL;
+        free(p);
+    }
+}
+
+void Diff::free_hash(hashtable** _htable,int size){
+    for(int i=0;i<size;i++){
+        free(_htable[i]);
+    }
+    free(_htable);
+}
+
+void Diff::free_table(biglinkedtable*** _table,int _linesA, int _linesB){
+    for(int i=0;i<_linesA;i++){
+        if(_table[i]!=NULL)free(_table[i]);
+    }
+    free(_table);
+}
+
+char* Diff::lcs(char *_arqA, char *_arqB, int type){
     char *tmp;
-		if(use_pd){
-			table = (biglinkedtable***)malloc(sizeof(biglinkedtable**)*(sizeFileA+1));
-			printf("setting...\n");
-	 		memset(table, NULL, sizeof(biglinkedtable***)*(sizeFileA+1));
-			printf("ok\n");
-		}
-		printf("iniciando...\n");
     if (type == T_Bin)
-            tmp = lcsBin(arqA,0,arqB,0);
-    else
-            tmp = lcsTxt(arqA,0,arqB,0);
+        tmp = lcsBin(_arqA,0,_arqB,0);
+    else{
 
-		if(use_pd){
-			//free_table(table);
-		}
+        //tmp = lcsTxt(0,0);
+    }
     return tmp;
 }
 
--- patoAlgorithms/diff.h
+++ patoAlgorithms/diff.h
@@ -5,29 +5,47 @@
 using namespace std;
 
 struct biglinkedtable{
-	char *add; 
-	int length;
-	int i,j;
-	struct biglinkedtable *next;
+    int index;
+    int i,j;
+    struct biglinkedtable *next;
 };
-
 typedef struct biglinkedtable biglinkedtable;
+
+struct hashtable{
+    unsigned int hash;
+    int length;
+    char *line;
+};
+typedef struct hashtable hashtable;
+
+struct t_lcs{
+    int index;
+    struct t_lcs *next;
+};
+typedef struct t_lcs t_lcs;
 
 class Diff
 {
 private:
-		biglinkedtable ***table; 
+    biglinkedtable ***table;
+    hashtable **htableA,**htableB;
     bool empty;
     int type;
     ifstream *fileA,*fileB;
     unsigned int sizeFileA,sizeFileB;
+    int sizeHashA,sizeHashB;
     void calculateDiff();
     char* lcsBin(char*,unsigned long, char*,unsigned long);
-    char* lcsTxt(char*,unsigned long, char*,unsigned long);
+    t_lcs* lcsTxt(int,int,int*);
     char* lcs(char*, char*, int);
-		void free_table(biglinkedtable***);
+    hashtable** init_hash(char*,int,unsigned int,int*);
+    biglinkedtable*** build_linked_table();
+    void free_lcs(t_lcs*);
+    void free_hash(hashtable**,int);
+    void free_table(biglinkedtable***,int,int);
+    void add_to_table(int,int,int,int,int);
 public:
-		static int use_pd;
+    static int use_pd;
     static const int T_Bin = 0;
     static const int T_Txt = 1;
     Diff(ifstream*,ifstream*,int);
