6a42a3493a4c9c1c165a7d65156360a606f2257e
david.brto <david.brto@localhost>
2011-10-10 10:55:22 -0300

    redoing changes...

--- patoFS/patofs.cpp
+++ patoFS/patofs.cpp
@@ -4,3 +4,19 @@
 PatoFS::PatoFS()
 {
 }
+
+PatoFS* PatoFS::getInstance() {
+
+	if (mPato == NULL) {
+		mPato = new PatoFS();
+	}
+
+	return mPato();
+}
+
+void PatoFS::destroyInstance() {
+
+	if (mPato != NULL) {
+		delete mPato;
+	}
+}
