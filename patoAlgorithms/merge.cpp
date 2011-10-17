1211e2a179120f830e0c6500908b1962e45eb540
hbarbalho <hbarbalho@localhost>
2011-10-16 22:45:59 -0200

    Initial implementation of merge. And removed the include that does not works in pseudo operating systems

--- patoAlgorithms/merge.cpp
+++ patoAlgorithms/merge.cpp
@@ -1,20 +1,20 @@
+#include <string.h>
 #include "merge.h"
-
-Merge::Merge()
-{
-}
 
 Merge::Merge(char* _fileNameBase,char* _fileNameA,char* _fileNameB){
     diff = new Diff(_fileNameA,_fileNameB,Diff::T_Txt);
 
     Merge::mergeFile = NULL;
     ifstream _fileA,_fileB,_fileBase;
+    ofstream _mergeFile;
     Merge::fileA = &_fileA;
     Merge::fileB = &_fileB;
     Merge::fileBase = &_fileBase;
     Merge::fileA->open(_fileNameA,fstream::binary);
     Merge::fileB->open(_fileNameB,fstream::binary);
     Merge::fileBase->open(_fileNameBase,fstream::binary);
+    Merge::mergeFile = &_mergeFile;
+    Merge::mergeFile->open(_fileNameBase,fstream::binary);
     doMerge();
     Merge::fileA->close();
     Merge::fileB->close();
@@ -22,6 +22,39 @@ Merge::Merge(char* _fileNameBase,char* _fileNameA,char* _fileNameB){
 }
 
 void Merge::doMerge(){
+    int i=0;
+    int lastLine=-1;
+    while(diff->getDiffItem(i)!=NULL){
+        switch (diff->getDiffItem(i)->getType()){
+            case DiffItem::Action_Add:
+                doAdd(diff->getDiffItem(i));
+                break;
+            case DiffItem::Action_Delete:
+                doDeletion(diff->getDiffItem(i));
+                break;
+            case DiffItem::Action_Change:
+                doChange(diff->getDiffItem(i));
+                break;
+        }
+
+        i++;
+    }
+}
+
+void Merge::insertLines(DiffItem* item, int from,int to){
+    for(int i=from;i<=to;i++){
+        mergeFile->write(item->getLineA(i),sizeof(char)*strlen(item->getLineA(i)));
+        mergeFile->write("\n",sizeof(char));
+    }
+}
+
+void Merge::doAdd(DiffItem* item){
+}
+
+void Merge::doDeletion(DiffItem* item){
+}
+
+void Merge::doChange(DiffItem* item){
 
 }
 
@@ -29,6 +62,6 @@ Merge::~Merge(){
     if(mergeFile!=NULL)mergeFile->close();
 }
 
-ifstream Merge::getFile(){
+ofstream* Merge::getFile(){
     return mergeFile;
 }
