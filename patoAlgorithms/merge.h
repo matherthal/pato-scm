b8dcbc57dbc24a70a875255fa39fc54341fef82d
hbarbalho <hbarbalho@gmail.com>
2011-10-23 22:46:10 -0200

    implementacao inicial do merge

--- patoAlgorithms/merge.h
+++ patoAlgorithms/merge.h
@@ -6,17 +6,16 @@ class Merge
 {
 private:
     Diff *diff;
-    ifstream *fileA;
-    ifstream *fileBase;
-    ifstream *fileB;
     ofstream *mergeFile;
+    Lcs *lcs_fileA_base;
+    Lcs *lcs_fileB_base;
     void doMerge();
     void doAdd(DiffItem*);
     void doDeletion(DiffItem*);
     void doChange(DiffItem*);
     void insertLines(DiffItem*,int from,int to);
 public:
-    Merge(char* _fileBase,char* _fileA,char* _fileB);
+    Merge(const char* _fileBase,const char* _fileA,const char* _fileB);
     ~Merge();
     ofstream* getFile();
 };
