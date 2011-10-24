d177fd43d3d9ca86da2d433da3dd8a043f52ce2d
hbarbalho <hbarbalho@gmail.com>
2011-10-23 22:46:10 -0200

    implementacao inicial do merge

--- patoAlgorithms/merge.cpp
+++ patoAlgorithms/merge.cpp
@@ -1,29 +1,19 @@
 #include <string.h>
 #include "merge.h"
 
-Merge::Merge(char* _fileNameBase,char* _fileNameA,char* _fileNameB){
+Merge::Merge(const char* _fileNameBase,const char* _fileNameA, const char* _fileNameB){
     diff = new Diff(_fileNameA,_fileNameB);
 
-    Merge::mergeFile = NULL;
-    ifstream _fileA,_fileB,_fileBase;
-    ofstream _mergeFile;
-    Merge::fileA = &_fileA;
-    Merge::fileB = &_fileB;
-    Merge::fileBase = &_fileBase;
-    Merge::fileA->open(_fileNameA,fstream::binary);
-    Merge::fileB->open(_fileNameB,fstream::binary);
-    Merge::fileBase->open(_fileNameBase,fstream::binary);
-    Merge::mergeFile = &_mergeFile;
-    Merge::mergeFile->open(_fileNameBase,fstream::binary);
+    lcs_fileA_base = new Lcs(_fileNameBase,_fileNameA);
+    lcs_fileB_base = new Lcs(_fileNameBase,_fileNameB);
+    diff = new Diff(_fileNameA,_fileNameB);
     doMerge();
-    Merge::fileA->close();
-    Merge::fileB->close();
-    Merge::fileBase->close();
 }
 
 void Merge::doMerge(){
     int i=0;
     int lastLine=-1;
+    t_lcs *lcsB = lcs_fileB_base->get_lcs();
     while(diff->getDiffItem(i)!=NULL){
         switch (diff->getDiffItem(i)->getType()){
             case DiffItem::Action_Add:
@@ -32,7 +22,7 @@ void Merge::doMerge(){
             case DiffItem::Action_Delete:
                 doDeletion(diff->getDiffItem(i));
                 break;
-            case DiffItem::Action_Change:
+            case DiffItem::Action_Change:                
                 doChange(diff->getDiffItem(i));
                 break;
         }
@@ -43,23 +33,49 @@ void Merge::doMerge(){
 
 void Merge::insertLines(DiffItem* item, int from,int to){
     for(int i=from;i<=to;i++){
-        mergeFile->write(item->getLineA(i),sizeof(char)*strlen(item->getLineA(i)));
-        mergeFile->write("\n",sizeof(char));
+        printf("%s\n",item->getLineA(i));
+       // mergeFile->write(item->getLineA(i),sizeof(char)*strlen(item->getLineA(i)));
+       // mergeFile->write("\n",sizeof(char));
     }
 }
 
 void Merge::doAdd(DiffItem* item){
+    t_lcs *lcsA = lcs_fileA_base->get_lcs();
+    int is_in=1;
+    int indexA = item->getFromA();
+    while(lcsA!=NULL && indexA<item->getToA() && is_in){
+        if(indexA!=lcsA->indexA)
+            is_in = 0;
+        lcsA = lcsA->next;
+        indexA++;
+    }
+    if(is_in){
+        for(int i=item->getFromA();i<item->getToA();i++)
+            printf("%s\n",item->getLineA(i));
+    }
 }
 
 void Merge::doDeletion(DiffItem* item){
+
 }
 
 void Merge::doChange(DiffItem* item){
-
+    printf("<<<<<< A\n");
+    for(int i=item->getFromA();i<item->getToA();i++){
+        printf("%s\n",item->getLineA(i));
+    }
+    printf("========\n");
+    for(int i=item->getFromB();i<item->getToB();i++){
+        printf("%s\n",item->getLineB(i));
+    }
+    printf(">>>>>> B\n");
 }
 
 Merge::~Merge(){
     if(mergeFile!=NULL)mergeFile->close();
+    delete lcs_fileA_base;
+    delete lcs_fileB_base;
+    delete diff;
 }
 
 ofstream* Merge::getFile(){
