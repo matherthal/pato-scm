16317e8af67194502ddc413b9cfee9299b0b584b
tpacheco <tpacheco@OmegaRed>
2011-11-07 00:45:04 -0200

    100% compiling version TODO: patoserverapi.cpp!!!!!

--- patoServerAPI/patoserverapi.cpp
+++ patoServerAPI/patoserverapi.cpp
@@ -17,28 +17,38 @@ void PatoServerApi::checkout(int revision, QString path, QString username, QStri
 
     if (!dataModel->validateProject(path.toStdString()))
         return;
+
     if (!dataModel->validateUser(username.toStdString(),password.toStdString()))
         return;
+
     if (!dataModel->validateUserProject(username.toStdString(),password.toStdString(),path.toStdString()))
         return;
+
     string strUsername = username.toStdString();
     string strPassword = password.toStdString();
     string strPath = path.toStdString();
-    cout << dataModel->checkOut(strUsername,strPassword,strPath,revision,filePath) << endl;
+
+    //last parameter is incompatible!
+    //TODO out << dataModel->checkOut(strUsername,strPassword,strPath,revision,filePath) << endl;
 }
 
 void PatoServerApi::checkin(QString path, QString username, QString password) {
 
     if (!dataModel->validateProject(path.toStdString()))
         return;
+
     if (!dataModel->validateUser(username.toStdString(),password.toStdString()))
         return;
+
     if (!dataModel->validateUserProject(username.toStdString(),password.toStdString(),path.toStdString()))
         return;
+
     string message = NULL;
     string strUsername = username.toStdString();
     string strPath = path.toStdString();
-    cout << dataModel->checkIn(filePath,strPath,strUsername,message) << endl;
+
+    //first parameter is incompatible!
+    //TODO cout << dataModel->checkIn(filePath,strPath,strUsername,message) << endl;
 }
 
 
