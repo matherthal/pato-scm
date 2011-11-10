98604f66ccabc8dd4cc3148dcce9d97ef1dde0b4
matherthal <matherthal@localhost>
2011-11-10 14:47:07 -0200

    file names corrected, files from serverAPI moved to patoCommunication

--- patoCommunication/client.h
+++ patoCommunication/client.h
@@ -2,10 +2,22 @@
 #define PATOCOMMUNICATION_H
 
 #include "PatoCommunication_global.h"
+#include "../patoClientAPI/patoclientapi.h"
+#include "../patoServerAPI/patoserverapi.h"
 
-class Client {
+class PATOCOMMUNICATIONSHARED_EXPORT PatoCommunication {
+private:
+    PatoClientApi* patoClient;
+    PatoServerApi* patoServer;
+
+    QString getPath(QString adress);
+
 public:
-    Client();
+
+    void checkout(int revision, QString adress, QString username, QString password);
+    void checkin(QString address, QString username, QString password);
+
+    PatoCommunication();
 };
 
 #endif // PATOCOMMUNICATION_H
