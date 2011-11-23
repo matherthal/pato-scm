7a7472413ce51032bd740eee31776344b12aa8c2
PC <PC@PC-PC>
2011-11-23 00:02:38 -0200

    Adding a button to test the ceation of a repository through gui.

--- patoGUI/ui_mainwindow.h
+++ patoGUI/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Tue 15. Nov 15:25:12 2011
+** Created: Tue Nov 22 21:28:18 2011
 **      by: Qt User Interface Compiler version 4.7.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
